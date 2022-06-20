a1851439743907f3fb269527bdc7a71eba014c5a
BradP <bradleypotzka39@gmail.com>
2022-06-19 18:22:13 -0700

    tab list css changes

--- src/Shared/Work/WorkInfo.js
+++ src/Shared/Work/WorkInfo.js
@@ -4,24 +4,26 @@ import './WorkInfo.css';
 function WorkInfo(props) {
     return(
         <div className='job-panel' id={`panel-${props.id}`} role='tabpanel' tabIndex={0} aria-labelledby={`tab-${props.id}`} hidden={props.isActive ? false : true}>
-                <h3>
-                    <span>{props.position}</span>
-                    <span>@</span>
-                    <a href={`${props.link}`} className='text-decoration-none' target={"_blank"} rel='noopener noreferrer'>
+            <h3>
+                <span>{props.position}</span>
+                <span className='company'>
+                    &nbsp;@&nbsp;
+                    <a href={`${props.link}`} className='text-decoration-none inline-link' target={"_blank"} rel='noopener noreferrer'>
                         {props.company}
                     </a>
-                </h3>
-                
-                <p className='range'>{props.date}</p>
-                <ul>
-                    {
-                        props.responsibilities.map((element, index) =>
-                            <li key={index} className='description'>
-                                {element}
-                            </li>
-                        )
-                    }
-                </ul>
+                </span>
+            </h3>
+            
+            <p className='range'>{props.date}</p>
+            <ul>
+                {
+                    props.responsibilities.map((element, index) =>
+                        <li key={index} className='description'>
+                            {element}
+                        </li>
+                    )
+                }
+            </ul>
         </div>
     );
 }
