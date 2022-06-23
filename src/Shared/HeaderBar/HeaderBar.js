811ceb070bacf7ca70010ee68b33818e9d764932
Bradley Potzka <bpotzka@gmail.com>
2022-06-22 20:03:29 -0700

    custom nav bar
    - custom nav bar
    - email icon
    - nav bar css

--- src/Shared/HeaderBar/HeaderBar.js
+++ src/Shared/HeaderBar/HeaderBar.js
@@ -11,35 +11,40 @@ import './HeaderBar.css';
 
 function HeaderBar() {
     return (
-        <Navbar collapseOnSelect expand='lg' bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav" className="d-flex align-items-center justify-content-center">
-                <Nav>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#work">Experience</Nav.Link>
-                    <Nav.Link href="#projects">Work</Nav.Link>
-                    <Nav.Link href={`${process.env.PUBLIC_URL}/assets/files/resume/Bradley Potzka Resume.pdf`} target={"_blank"} rel='noreferrer'>Résumé</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            
-            <Nav className="d-flex justify-content-center">
-                <Nav.Item className='px-2 py-2'>
+        <header className='nav_StyledHeader'>
+            <nav className='nav_StyledNav'>
+                <div className='nav_StyledLinks'>
+                    <ol>
+                        <li style={{transitionDelay: 0}}>
+                            <a href='/#about'>About</a>
+                        </li>
+                        <li style={{transitionDelay: 100}}>
+                            <a href='/#work'>Experience</a>
+                        </li>
+                        <li style={{transitionDelay: 200}}>
+                            <a href='/#projects'>Projects</a>
+                        </li>
+                    </ol>
+                    <div>
+                        <a class='resume-button' href={`${process.env.PUBLIC_URL}/assets/files/resume/Bradley Potzka Resume.pdf`} target={"_blank"} rel='noreferrer'>Resume</a>
+                    </div>
+                </div>
+                <div className='nav_StyledImages'>
+                    <a href="mailto:bradleypotzka39@gmail.com" target={"_blank"} rel='noreferrer'>
+                        <img className='header-image' src={`${process.env.PUBLIC_URL}/assets/images/socials/Mail/mail.png`} />
+                    </a>
                     <a href="https://github.com/Phyrol" target={"_blank"} rel='noreferrer'>
-                        <Image src={`${process.env.PUBLIC_URL}/assets/images/socials/GitHub-Mark/PNG/GitHub-Mark-Light-32px.png`} />
+                        <img className='header-image' src={`${process.env.PUBLIC_URL}/assets/images/socials/GitHub-Mark/PNG/GitHub-Mark-Light-32px.png`} />
                     </a>
-                </Nav.Item>
-                <Nav.Item className='px-2 py-2'>
-                    <a href="https://phyrol.itch.io/" target={"_blank"} rel='noreferrer'>
-                        <Image height={32} width={32} src={`${process.env.PUBLIC_URL}/assets/images/socials/Itchio/logos/itchio-logo-textless-white.png`} />
+                    <a href="http://phyrol.itch.io/" target={"_blank"} rel='noreferrer'>
+                        <img className='header-image' src={`${process.env.PUBLIC_URL}/assets/images/socials/Itchio/logos/itchio-logo-textless-white.png`} />
                     </a>
-                </Nav.Item>
-                <Nav.Item className='px-2 py-2'>
                     <a href="https://www.linkedin.com/in/bradleypotzka/" target={"_blank"} rel='noreferrer'>
-                        <Image size='tester' width={32} src={`${process.env.PUBLIC_URL}/assets/images/socials/LinkedIn/linkedIn.png`} />
+                        <img className='header-image' src={`${process.env.PUBLIC_URL}/assets/images/socials/LinkedIn/linkedIn.png`} />
                     </a>
-                </Nav.Item>
-            </Nav>
-        </Navbar>
+                </div>
+            </nav>
+        </header>
     );
 }
 
