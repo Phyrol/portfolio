ee73c7a2b457276a3d2ea5ff2334677b633988ee
Bradley Potzka <bpotzka@gmail.com>
2022-06-22 20:19:51 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Home from './HomePage/Home';
 import HeaderBar from './Shared/HeaderBar/HeaderBar';
+import Footer from './Shared/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='layout_StyledContent'>
         <HeaderBar />
         <Home />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Footer.css';
+
+function Footer() {
+    return(
+        <footer>
+            <div className='footer_StyledCredit'>
+                <div>
+                    <a href='https://github.com/Phyrol/portfolio' target={"_blank"} rel='noreferrer'>
+                        Built by Bradley Potzka
+                    </a>
+                </div>
+                <div>
+                    <a href='https://brittanychiang.com/#about' target={"_blank"} rel='noreferrer'>
+                        Design Inspired by Brittany Chiang
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
