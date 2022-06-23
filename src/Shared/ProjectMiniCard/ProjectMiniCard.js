47aa4c5866caa4f63fece09a78ffcada28a56df7
Bradley Potzka <bpotzka@gmail.com>
2022-06-22 18:50:17 -0700

    custom project cards

--- src/Shared/ProjectMiniCard/ProjectMiniCard.js
+++ src/Shared/ProjectMiniCard/ProjectMiniCard.js
@@ -1,72 +1,72 @@
 import React from 'react';
 import './ProjectMiniCard.css';
 import { Card, Col, Row } from 'react-bootstrap';
+import { LinkExternalIcon, MarkGithubIcon } from '@primer/octicons-react';
 
 function ProjectMiniCard(props) {
 
     let thumbnailDisplay =
-        <Col md='4' className='d-flex align-items-center justify-content-center'>
+        <div className='thumbnail'>
             <a href={`${props.download}`} target={"_blank"} rel='noreferrer'>
-                <Card.Img className='thumbnail' src={props.src} />
-            </a>            
-        </Col>
+                <img src={props.src} />
+            </a> 
+        </div>
+                   
 
     let infoDisplay =
-        <Col md='6' className='d-flex align-items-center justify-content-center info'>
-            <Card.Body>
-                <div className='d-flex align-items-center'>
-                    <a href={`${props.download}`} target={"_blank"} rel='noreferrer' className='text-decoration-none'>
-                        <Card.Title className='title'>{props.title}</Card.Title>
+        <div className='content'>
+            <div className='d-flex align-items-center'>
+                <h3 className='title'>
+                    <a href={`${props.download}`} target={"_blank"} rel='noreferrer'>
+                        {props.title}
                     </a>
+                </h3>
 
-                    <Card.Text className='tools'>
-                        {props.tools}
-                    </Card.Text>
-                </div>              
-                
-                <ul>
+                <p className='tools'>
+                    {props.tools}
+                </p>
+            </div>              
+            
+            <ul>
+                <p className='col align-self-center'>
                     <li>
-                        <Card.Text>
-                            Shipped: {props.shipped}
-                        </Card.Text>
+                        Shipped: {props.shipped}
                     </li>
                     <li>
-                        <Card.Text>
-                            Team Size: {props.teamSize}
-                        </Card.Text>
+                        Team Size: {props.teamSize}
                     </li>
                     <li>
-                        <Card.Text>
-                            Platform: {props.platform}
-                        </Card.Text>
+                        Platform: {props.platform}
                     </li>
                     <li>
-                        <Card.Text>
-                            Position: {props.position}
-                        </Card.Text>
+                        Position: {props.position}
                     </li>
                     <li>
-                        <Card.Text>
-                            Genre: {props.genre}
-                        </Card.Text>
+                        Genre: {props.genre}
                     </li>
-                </ul>
-                <Card.Link href={`${props.github}`} target={"_blank"} rel='noreferrer'>GitHub</Card.Link>
-                <Card.Link href={`${props.download}`} target={"_blank"} rel='noreferrer'>Download</Card.Link>
-            </Card.Body> 
-        </Col>
+                </p>
+            </ul>
+            <div className='project-links'>
+                <span>
+                    <a href={`${props.github}`} target={"_blank"} rel='noreferrer'>
+                        <MarkGithubIcon size={24} />
+                    </a>
+                    <a href={`${props.download}`} target={"_blank"} rel='noreferrer'>
+                        <LinkExternalIcon size={24} />
+                    </a>
+                </span>
+            </div>
+        </div>
 
 return(
-    <Card className='project-card'>
-        <Row>
+    <div className='project-card'>
             {props.imageIsLeft ?
                 thumbnailDisplay : infoDisplay
             }
             {props.imageIsLeft ?
                 infoDisplay : thumbnailDisplay
             }
-        </Row>
-    </Card>
+    </div>
 );
 }
 
